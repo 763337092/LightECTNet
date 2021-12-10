a95b2f9318e544b322828fd7bc39d4999ae6caa5
Lindada <amos_da_li@163.com>
2021-12-10 10:45:43 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -18,6 +18,10 @@ python title_w2v_feat_new.py
 python make_seg_feat.py
 
 # train models (largest model takes about 40h on single P40)
+CUDA_VISIBLE_DEVICES=0 python run_LightECTNet.py > LightECTNet_log.txt &
+
+# other models
+'''
 CUDA_VISIBLE_DEVICES=0 python final_enhancedrcnn1001.py > log0.txt &
 CUDA_VISIBLE_DEVICES=1 python final_esim1011_5.py > log1.txt &
 CUDA_VISIBLE_DEVICES=2 python final_gru_lxmert1001.py > log2.txt &
@@ -26,4 +30,4 @@ CUDA_VISIBLE_DEVICES=4 python final_lstm_nextvlad_siamese1001.py > log4.txt &
 CUDA_VISIBLE_DEVICES=5 python final_lstm1001.py > log5.txt &
 CUDA_VISIBLE_DEVICES=6 python final_lxmert1001.py > log6.txt &
 CUDA_VISIBLE_DEVICES=7 python final_light_lxmert1001.py > log7.txt &
-
+'''
